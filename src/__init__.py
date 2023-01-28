e8806dab203a917e2649b18f78274b99b1ebcc81
Namah Shrestha <namah.shrestha@biofourmis.com>
2023-01-28 18:08:12 +0530

    Added instance message on init. Added constructor on instance manager

---
+++ src/__init__.py
@@ -0,0 +1,34 @@
+# builtins
+import json
+import abc
+
+# modules
+import src.constants as constants
+
+
+class Instance:
+    def __init__(self, instance_hash: str) -> None:
+        self.instance_hash: str = instance_hash
+
+    def handle(self) -> list:
+        pass
+
+
+class InstanceMessage:
+    @staticmethod
+    def is_schema_valid(message: str) -> bool:
+        try:
+            json_message: dict = json.loads(message)
+            command: str = json_message.get(constants.COMMAND)
+            instance_hash: str = json_message.get(constants.INSTANCE_HASH)
+            if not command or not instance_hash:
+                return False
+            return True
+        except json.JSONDecodeError:
+            raise json.JSONDecodeError(
+                "Invalid message format. Message needs to have command and instance hash"
+            )
+
+    @staticmethod
+    def decode_message(message: str) -> str:
+        return json.loads(message)
--- src/instance_manager.py
+++ src/instance_manager.py
@@ -1,7 +1,15 @@
+# builtins
 import os
 
+# module
+import src
 
-class InstanceManager:
+
+class InstanceManager(src.Instance):
+    def __init__(self, command: str, instance_hash: str) -> None:
+        super().__init__(instance_hash)
+        self.command: str = command
+
     def create_instance(self) -> None:
         """
         1. Build the image from the dockerfile.
@@ -10,7 +18,9 @@ class InstanceManager:
         """
         try:
             os.system("docker image build . -t centos-demo:latest -f Dockerfile.centos")
-            os.system("docker container run --name centos_demo -d centos-demo:latest")
+            os.system(
+                f"docker container run --name centos_demo_{self.instance_hash} -p 0.0.0.0:7777:22 -d centos-demo:latest"
+            )
         except Exception as e:
             raise Exception(e)
 
@@ -22,11 +32,19 @@ class InstanceManager:
         """
         try:
             os.system(
-                "docker container stop $(docker container ls -aq --filter 'name=centos_demo')"
+                f"docker container stop $(docker container ls -aq --filter 'name=centos_demo_{self.instance_hash}')"
             )
             os.system(
-                "docker container rm $(docker container ls -aq --filter 'name=centos_demo')"
+                f"docker container rm $(docker container ls -aq --filter 'name=centos_demo_{self.instance_hash}')"
             )
             os.system("docker image rm -f centos-demo:latest")
         except Exception as e:
             raise Exception(e)
+
+    def handle(self) -> list:
+        if self.command == src.constants.CREATE:
+            self.create_instance()
+            return [0]
+        elif self.command == src.constants.DELETE:
+            self.delete_instance()
+            return [2]
