7cb3b8fa59f6673055d8180a0eee8f79a545bcf2
Namah Shrestha <namah.shrestha@biofourmis.com>
2023-01-30 16:32:25 +0530

    Completed unit test for instance manager

--- tests/unit/test_instance_manager.py
+++ tests/unit/test_instance_manager.py
@@ -11,11 +11,66 @@ import typing
 
 # modules
 import src.instance_manager as im
+import src.constants as constants
 
 
-class TestInstance(unittest.TestCase):
+class BaseTestInstanceManager:
+    """
+    Base Test Instance Manager for setting instance manager object.
+    Since the method is common, a common class is used for it.
+
+    Author: Namah Shrestha
+    """
+
+    def __init__(
+        self,
+        command: str,
+        instance_hash: str,
+        image_name: str,
+        image_tag: str,
+        container_name: str,
+        dockerfile_name: str,
+        filter_container_command: str,
+        instance_mgr_obj: im.InstanceManager,
+    ) -> None:
+        self.command: str = command
+        self.instance_hash: str = instance_hash
+        self.image_name: str = image_name
+        self.image_tag: str = image_tag
+        self.container_name: str = container_name
+        self.dockerfile_name: str = dockerfile_name
+        self.filter_container_command: str = filter_container_command.format(
+            self.container_name
+        )
+        self.instance_mgr_obj: im.InstanceManager = instance_mgr_obj
+
+    def extract_cmd_from_mock_call_str(self, mock_call_str) -> str:
+        """
+        Turn call(<command>) to <command>
+        """
+        mock_call_str = mock_call_str.replace("call(", "")
+        mock_call_str = mock_call_str[:-1]
+        return eval(mock_call_str)
+
+
+class TestCentosInstanceManager(unittest.TestCase, BaseTestInstanceManager):
+    """
+    Centos Instance manager test cases
+
+    Author: Namah Shrestha
+    """
+
     def setUp(self) -> None:
-        self.instance_mgr_obj: im.InstanceManager = im.InstanceManager()
+        self.instance_hash: str = "test_centos_instance_hash"
+        self.image_name: str = constants.CENTOS_IMAGE_NAME
+        self.image_tag: str = constants.CENTOS_IMAGE_TAG
+        self.container_name: str = constants.CENTOS_CONTAINER_NAME.format(
+            self.instance_hash
+        )
+        self.dockerfile_name: str = constants.CENTOS_DOCKERFILE_NAME
+        self.filter_container_command: str = constants.CENTOS_FILTER_CONTAINER.format(
+            self.container_name
+        )
 
     @mock.patch("os.system")
     def test_creation(self, mock_system) -> None:
@@ -24,13 +79,19 @@ class TestInstance(unittest.TestCase):
 
         Author: Namah Shrestha
         """
+        self.command = "CREATE"
+        self.instance_mgr_obj: im.InstanceManager = im.CentosInstanceManager(
+            self.command, self.instance_hash
+        )
+        super(BaseTestInstanceManager, self).__init__()
         self.instance_mgr_obj.create_instance()
         result: typing.List = [str(call) for call in mock_system.mock_calls]
+        result = list(map(lambda x: self.extract_cmd_from_mock_call_str(x), result))
         self.assertEqual(
             result,
             [
-                "call('docker image build . -t centos-demo:latest -f Dockerfile.centos')",
-                "call('docker container run --name centos_demo -d centos-demo:latest')",
+                f"docker image build . -t {self.image_name}:{self.image_tag} -f {self.dockerfile_name}",
+                f"docker container run --name {self.container_name} -d {self.image_name}:{self.image_tag}",
             ],
         )
 
@@ -41,13 +102,19 @@ class TestInstance(unittest.TestCase):
 
         Author: Namah Shrestha
         """
+        self.command = "DELETE"
+        self.instance_mgr_obj: im.InstanceManager = im.CentosInstanceManager(
+            self.command, self.instance_hash
+        )
+        super(BaseTestInstanceManager, self).__init__()
         self.instance_mgr_obj.delete_instance()
         result: typing.List = [str(call) for call in mock_system.mock_calls]
+        result = list(map(lambda x: self.extract_cmd_from_mock_call_str(x), result))
         self.assertEqual(
             result,
             [
-                "call(\"docker container stop $(docker container ls -aq --filter 'name=centos_demo')\")",
-                "call(\"docker container rm $(docker container ls -aq --filter 'name=centos_demo')\")",
-                "call('docker image rm -f centos-demo:latest')",
+                f"docker container stop $({self.filter_container_command})",
+                f"docker container rm $({self.filter_container_command})",
+                f"docker image rm -f {self.image_name}:{self.image_tag}",
             ],
         )
