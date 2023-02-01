a622ecc09d114b512988f6df1e8746ce5a52ed91
Namah Shrestha <namah.shrestha@biofourmis.com>
2023-02-01 10:40:43 +0530

    Added constants. Completed constants

--- src/constants.py
+++ src/constants.py
@@ -1,18 +1,27 @@
 """
 Constants for the applicationsrc/instance_exec.py
+
+Author: Namah Shrestha
 """
 
-COMMAND = "command"
-INSTANCE_HASH = "instance_hash"
+# Keys
+INSTANCE_OS: str = "instance_os"
+COMMAND: str = "command"
+INSTANCE_HASH: str = "instance_hash"
+EXEC_COMMAND: str = "exec_command"
 
+# SUPPORTED COMMANDS
+CREATE: str = "CREATE"
+EXECUTE: str = "EXEC"
+DELETE: str = "DELETE"
 
-CREATE = "CREATE"
-EXECUTE = "EXEC"
-DELETE = "DELETE"
+# SUPPORTED OS
+CENTOS: str = "centos"
+SUPPORTED_OS: list = [CENTOS]
 
 # CENTOS IMAGE DEVELOPMENT
-CENTOS_IMAGE_NAME = "centos-demo"
-CENTOS_IMAGE_TAG = "latest"
-CENTOS_DOCKERFILE_NAME = "Dockerfile.centos"
-CENTOS_CONTAINER_NAME = "centos_demo_{}"
-CENTOS_FILTER_CONTAINER = "docker container ls --aq --filter 'name={}'"
+CENTOS_IMAGE_NAME: str = "centos-demo"
+CENTOS_IMAGE_TAG: str = "latest"
+CENTOS_DOCKERFILE_NAME: str = "Dockerfile.centos"
+CENTOS_CONTAINER_NAME: str = "centos_demo_{}"
+CENTOS_FILTER_CONTAINER: str = "docker container ls --aq --filter 'name={}'"
